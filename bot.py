491aa37626dec4c45e4ea8b203df9d72a5332425
psuedoo <forthewin720@gmail.com>
2020-10-04 22:32:15 -0400

    added dad jokes

--- bot.py
+++ bot.py
@@ -9,36 +9,36 @@ GUILD = os.environ['DISCORD_GUILD']
 token=os.environ["TOKEN"]
 guild=os.environ["DISCORD_GUILD"] 
 
-client = discord.Client()
+# client = discord.Client()
 
-def get_prefix(bot, message):
-    prefixes = ['?']
 
-    if not message.guild:
-        return '?'
+intents = discord.Intents.default()
+intents.members = True
 
-    return commands.when_mentioned_or(*prefixes)(bot, message)
+bot = commands.Bot(command_prefix='?', intents=intents)
 
 initial_extensions = ["cogs.basic","cogs.admin"]
 
-bot = commands.Bot(command_prefix=get_prefix)
-
-
-
-
 @bot.event
 async def on_ready():
-    print(f"Logged in as: {bot.user.name}")
+    print(f"Logged in")
+#    game = discord.Game("Coding Myself", type=1, url="https://twitch.tv/psuedoo")
+#    await bot.change_presence(activity=game)
 
-    game = discord.Game("Coding Myself", type=1, url="https://twitch.tv/psuedoo")
-    await bot.change_presence(activity=game)
-    
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        to_send = f"Welcome {member.mention} to {guild.name}!"
+        await guild.system_channel.send(to_send)
 
+@bot.command(name="test")
+async def test(ctx):
+    await ctx.send("The test has passed!")
 
 
 if __name__ == '__main__':
     for extension in initial_extensions:
         bot.load_extension(extension)
-
 
 bot.run(token)
--- cogs/basic.py
+++ cogs/basic.py
@@ -1,5 +1,6 @@
 import random
 import discord
+import requests
 from discord.ext import commands
 
 class Basic(commands.Cog):
@@ -9,6 +10,17 @@ class Basic(commands.Cog):
     @commands.command(name="randomnumber", aliases=["randnum",])
     async def randomnumber(self, ctx, max_number=1000):
         await ctx.send(random.randint(1, max_number))
+
+    @commands.command(name="randomchoice", aliases=["rc",])
+    async def randomchoice(self, ctx, *choices):
+        await ctx.send(f"I have chosen {random.choice(choices)}.")
+
+    @commands.command(name="joke")
+    async def joke(self, ctx):
+        url = "https://icanhazdadjoke.com/"
+        headers = {"Accept": "application/json"}
+        response = requests.get(url, headers=headers)
+        await ctx.send(response.json()['joke'])
 
 def setup(bot):
     bot.add_cog(Basic(bot))

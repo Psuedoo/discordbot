15cc104ea604af6409a834668ab7cf9c9efa1845
psuedoo <forthewin720@gmail.com>
2020-10-05 02:59:09 -0400

    wrote some TODOs and created a (hopeful) pseudo-guide

--- cogs/basic.py
+++ cogs/basic.py
@@ -8,6 +8,24 @@ class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # TODO : Stream content for 10/5 : 10/6 night: 
+    # TODO : Create commands to edit the config
+    # TODO :    -- ?setmsgid <message_id>
+    # TODO :    -- ?setchannel <#channel>
+    # TODO :    -- ?createroll <role_name>
+    # TODO :    -- ?linkemoji <role_name> <emoji=None> (if emoji=None, pick random one from emoji list in config)
+
+    # TODO : Then and only then, after all the above are finished, we can fix the listener for assigning roles
+    #           based on reaction to a message. We could do it normally, but we want it to be dynamic across servers.
+
+
+    # Bot has to:
+    # find channel.message to attach reactions to
+    # tie certain reactions to certain roles
+    # listen for users to click reaction
+    # if user clicks reaction, assign corresponding role
+
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(*reactions):
        

b548048aa078592a12f9824d04e3f9453eee4a11
psuedoo <forthewin720@gmail.com>
2020-10-13 00:54:30 -0400

    sound download/view commands added; sound db created

--- cogs/sound.py
+++ cogs/sound.py
@@ -1,4 +1,6 @@
+import os
 import discord
+from tinydb import TinyDB, Query 
 from config import Config
 from sound_class import SoundFile
 from cogs.utils import checks
@@ -25,6 +27,8 @@ class Sound(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.voice_client = None
+        self.db = TinyDB(os.path.expanduser('~/coding/sounds/sounds.json'))
+
 
     @commands.command(name="join")
     async def join(self, ctx):
@@ -35,7 +39,21 @@ class Sound(commands.Cog):
     async def leave(self, ctx):
         channel = ctx.author.voice.channel
         await self.voice_client.disconnect()
-         
+
+    @commands.check(checks.is_bot_enabled)
+    @commands.command(name="soundadd")
+    async def soundadd(self, ctx, name, url):
+        sound = SoundFile(name, url, title=name)
+        sound.download_sound()
+        await ctx.send(f"Added {name} to sounds!")
+
+    @commands.check(checks.is_bot_enabled)
+    @commands.command(name="viewsounds")
+    async def viewsounds(self, ctx):
+        sounds = []
+        for sound in self.db:
+            sounds.append(sound)
+        await ctx.send(sounds)
 
 def setup(bot):
     bot.add_cog(Sound(bot))
--- sound_class.py
+++ sound_class.py
@@ -1,8 +1,12 @@
+import os
 import youtube_dl
+from pathlib import Path
+from tinydb import TinyDB, Query
 
 
 class SoundFile:
-    def __init__(self, url, title=None):
+    def __init__(self, command_name, url, title=None):
+        self.command_name = command_name
         self.url = url
         if title:
             self.title = title
@@ -10,10 +14,42 @@ class SoundFile:
             self.title = '%(title)s'
 
         self.ydl_opts = {'format': 'bestaudio',
-                        'outtmpl': f'{self.title}.%(ext)s',
+                        'noplaylist': True,
+                        'outtmpl': f'~/coding/sounds/{self.title}.%(ext)s',
                         'postprocessors': [{'key': 'FFmpegExtractAudio'}]}
+
+        p = Path('~')
+        self.path = p / 'coding' / 'sounds' / 'sounds.json'
+        self.db = TinyDB(os.path.expanduser(self.path))
+        
 
     def download_sound(self):
         with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
             ydl.download([self.url])
+
+        self.add_command()
+
+    def add_command(self):
+        command_info = {'file': f'~/coding/sounds/{self.title}.opus',
+                        'command_name': str(self.command_name)}
+
+        self.db.insert(command_info)
+
+    def view_commands(self):
+        for command in self.db:
+            print(command)
+            
+if __name__ == '__main__':
+    command_name = input("Enter a command name: ")
+    url = input("Please enter a url: ")
+    title = input("Please enter a title, or N: ")
+
+    if title.lower() != "n":
+        sound = SoundFile(command_name, url, title)
+    else:
+        sound = SoundFile(command_name, url)
+
+    sound.download_sound()
+
+    sound.view_commands()
 

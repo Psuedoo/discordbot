6cec435263952494cb559e9b86e62187c0065b4d
psuedoo <forthewin720@gmail.com>
2020-10-22 01:25:14 -0400

    transitioning polls to use poll object

--- cogs/poll.py
+++ cogs/poll.py
@@ -2,7 +2,7 @@ import os
 import discord
 import asyncio
 import json
-from tinydb import TinyDB, Query 
+from pathlib import Path
 from config import Config
 from command_class import CustomCommand
 from cogs.utils import checks
@@ -10,14 +10,71 @@ from discord.ext import commands
 from discord.abc import Messageable
 from discord.utils import get
 
-class Poll(commands.Cog):
+
+class Poll:
+    def __init__(self, name, author=None, message=None):
+        self.path = Path('.') / 'polls' / f'{name}_poll.json'
+        if self.path.is_file():
+            poll_data = self.get_poll_data()
+        else:
+            poll_data = {}
+
+        self.name = poll_data.get('name', name)
+        self.author_id = poll_data.get('author_id', author.id)
+        self.author_name = poll_data.get('author_name', author.display_name)
+        self.message_id = poll_data.get('message_id', message.id)
+        self.message_channel_id = poll_data.get('message_channel_id', message.channel.id)
+        self.duration = poll_data.get('duration', 180)
+        self.options = poll_data.get('options', {}) # Dict with option names: {emoji, voter list}
+
+        if len(poll_data) == 0:
+            self.update_poll()
+
+    def to_json(self):
+        poll_data = {
+                "name": self.name,
+                "author_id": self.author_id,
+                "author_name": self.author_name,
+                "message_id": self.message_id,
+                "message_channel_id": self.message_channel_id,
+                "duration": self.duration,
+                "options": self.options # Dict with option names: {emoji, voter list}
+        }
+        return json.dumps(poll_data, indent=2)
+
+
+    def update_poll(self):
+        with open(self.path.absolute(), "w") as poll_file:
+            poll_file.write(self.to_json())
+
+    def get_poll_data(self):
+        try:
+            with open(self.path.absolute()) as poll_file:
+                return json.load(poll_file)
+        except FileNotFoundError:
+            self.update_poll()
+               
+
+class PollCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.guilds = self.bot.guilds
-        self.poll_file = open("poll_file.json", 'w')
+        self.poll_dir = f"{os.path.expanduser('~/coding/discordbot/polls')}"
+
+
+    def add_poll(self):
+        with open(self.poll_file, "a") as poll_file:
+            json.dump(self.poll_data, poll_file, indent=2)
 
     def update(self):
-        self.poll_file.write(json.dumps(self.poll_data), indent=2)
+        with open(self.poll_file, "w") as poll_file:
+            json.dump(self.poll_data, poll_file, indent=2)
+        self.poll_push()
+
+    def poll_push(self):
+        with open(self.poll_file) as poll_file:
+            poll_data = json.load(poll_file)
+        print(poll_data)
     
     @commands.Cog.listener(name="on_reaction_add")
     async def on_vote(self, reaction, user):
@@ -27,24 +84,14 @@ class Poll(commands.Cog):
         pass
 
     @commands.command(name="pollcreate")
-    async def create_poll(self, ctx, poll_name, duration=60):
+    async def create_poll(self, ctx, poll_name):
         message = await ctx.send(f"Poll: {poll_name}")
 
-        self.poll_data = {'poll_name': poll_name,
-                'author': ctx.author,
-                'duration': duration,
-                'poll_message_id': message.id, 
-                'options': {},
-                }
-
-        self.update()
+        poll = Poll(poll_name, ctx.message.author, message)
 
     @commands.command(name="polladd")
     async def add_poll_option(self, ctx, option, emoji):
-        options = self.poll_data.get('options')
-        options[option] = {'emoji': emoji, 'voters': []}
-        self.update()
-
+        pass        
 
     @commands.command(name="pollend")
     async def end_poll(self, ctx, poll_name=None):
@@ -52,4 +99,4 @@ class Poll(commands.Cog):
         pass
 
 def setup(bot):
-    bot.add_cog(Poll(bot))
+    bot.add_cog(PollCog(bot))

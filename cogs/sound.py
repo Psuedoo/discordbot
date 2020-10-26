ecbd3e8bf3b354022131dc8c8dca6b77504b7277
Psuedoo <43244534+Psuedoo@users.noreply.github.com>
2020-10-25 23:04:59 -0400

    refactored sound_class file into cogs/sound

--- cogs/sound.py
+++ cogs/sound.py
@@ -1,14 +1,12 @@
 import os
-import re
 import discord
 import asyncio
+import youtube_dl
+from pathlib import Path
 from tinydb import TinyDB, Query
 from config import Config
-from sound_class import SoundFile
 from cogs.utils import checks
 from discord.ext import commands
-from discord.abc import Messageable
-from discord.utils import get
 
 
 def instantiate_configs(guilds):
@@ -17,6 +15,58 @@ def instantiate_configs(guilds):
 
 def instantiate_dbs(guilds):
     return [TinyDB(Config(guild).sounds) for guild in guilds if guild]
+
+
+class SoundFile:
+    def __init__(self, guild, command_name=None, url=None, title=None):
+        self.command_name = command_name
+        self.url = url
+        self.guild = guild
+        self.guild_id = guild.id
+        self.config = Config(guild)
+        self.path = Path.cwd() / 'sounds'
+        if not self.path.is_dir():
+            self.path.mkdir()
+
+        if title:
+            self.title = title
+        else:
+            self.title = '%(title)s'
+
+        self.file_path = self.path / self.title
+        self.db_path = self.path / f'sounds_{self.guild_id}.json'
+
+        self.ydl_opts = {'format': 'bestaudio',
+                         'noplaylist': True,
+                         'outtmpl': f'{self.file_path}.%(ext)s',
+                         'postprocessors': [{'key': 'FFmpegExtractAudio'}]}
+
+        self.db = TinyDB(self.db_path)
+        self.config.sounds = self.db_path
+        self.config.update_config()
+
+    def download_sound(self):
+        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+            ydl.download([self.url])
+
+        for file in self.path.iterdir():
+            if file.stem == self.file_path.stem:
+                self.file_path = file
+
+    def add_command(self):
+        if not self.file_path.is_file():
+            self.download_sound()
+
+        if self.command_name in [sound.get('command_name') for sound in self.db]:
+            return
+        else:
+
+            config = Config(self.guild)
+            command_info = {'file': str(self.file_path),
+                            'command_name': str(self.command_name)}
+
+            self.db.insert(command_info)
+            config.update_config()
 
 
 class Sound(commands.Cog):

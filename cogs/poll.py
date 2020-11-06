94e09ad7f18d6e3615daa54163417652e42e4299
Psuedoo <43244534+Psuedoo@users.noreply.github.com>
2020-11-06 00:58:55 -0500

    progress for polls

--- cogs/poll.py
+++ cogs/poll.py
@@ -12,6 +12,18 @@ class Poll(commands.Cog):
     async def poll_create(self, ctx):
         pass
 
+    @commands.command(name="pollend")
+    async def poll_end(self, ctx):
+        pass
+
+    @commands.command(name="addchoice")
+    async def poll_add_choice(self, ctx):
+        pass
+
+    @commands.command(name="removechoice")
+    async def poll_remove_choice(self, ctx):
+        pass
+
 
 def setup(bot):
     bot.add_cog(Poll(bot))

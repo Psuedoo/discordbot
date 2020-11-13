77dd50beed8492bd299aa8bb5cd6156f44a3d419
Psuedoo <43244534+Psuedoo@users.noreply.github.com>
2020-11-13 00:29:08 -0500

    finished role integration with db

--- cogs/role.py
+++ cogs/role.py
@@ -15,13 +15,15 @@ class ReactionRole(commands.Cog):
         self.hidden = False
 
     # TODO : Added listener for un-react and remove the corresponding role
-    async def role_embed_remove(self, config, del_role=None, emoji=None):
-        for role in config.roles.values():
+    async def role_embed_remove(self, guild, del_role=None, emoji=None):
+        for role in await db.db_handler_role.get_emoji_roles(guild):
             if role.get('emoji'):
                 if emoji and role.get('emoji') == emoji or del_role and role.get('name') == del_role.name:
                     emoji = role.get('emoji')
-                    channel = await self.bot.fetch_channel(config.role_message_channel_id)
-                    message = await channel.fetch_message(config.role_message_id)
+                    channel_id, message_id = await db.db_handler_role.get_channel(guild)
+                    if channel_id and message_id:
+                        channel = await self.bot.fetch_channel(channel_id)
+                        message = await channel.fetch_message(message_id)
 
                     role_embed = message.embeds[0]
                     for index, field in enumerate(role_embed.fields):
@@ -31,7 +33,7 @@ class ReactionRole(commands.Cog):
 
                     await message.remove_reaction(emoji, self.bot.user)
                     return {'name': role.get('name'), 'emoji': emoji}
-                    config.update_config()
+
                     break
         return None
 
@@ -41,115 +43,69 @@ class ReactionRole(commands.Cog):
             return
         else:
             guild = await self.bot.fetch_guild(reaction.guild_id)
-            current_config = Config(guild)
-
-            message_id = current_config.role_message_id
-            channel_id = current_config.role_message_channel_id
+            channel_id, message_id = await db.db_handler_role.get_channel(guild)
 
             if message_id == reaction.message_id and channel_id == reaction.channel_id:
 
-                # Reactor's User ID
-                user_id = reaction.user_id
-
-                emoji = reaction.emoji
-
-                user = reaction.member
-
-                for role in current_config.roles.values():
-                    if emoji.name == role.get('emoji'):
+                for role in await db.db_handler_role.get_emoji_roles(guild):
+                    if reaction.emoji.name == role.get('emoji'):
                         # Assigns role to corresponding reacted emoji
 
                         roles = discord.utils.get(guild.roles, name=role.get('name'))
-                        await user.add_roles(roles)
-                        print(f"Assigned {role.get('name')} to {user_id}.")
+                        await reaction.member.add_roles(roles)
+                        print(f"Assigned {role.get('name')} to {reaction.user_id}.")
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, reaction):
 
         guild = await self.bot.fetch_guild(reaction.guild_id)
-        current_config = Config(guild)
 
-        message_id = current_config.role_message_id
-        channel_id = current_config.role_message_channel_id
+        channel_id, message_id = await db.db_handler_role.get_channel(guild)
 
         if message_id == reaction.message_id and channel_id == reaction.channel_id:
 
-            # Reactor's User ID
-            user_id = reaction.user_id
+            user = await guild.fetch_member(reaction.user_id)
 
-            emoji = reaction.emoji
-
-            user = await guild.fetch_member(user_id)
-
-            for role in current_config.roles.values():
-                if emoji.name == role.get('emoji'):
+            for role in await db.db_handler_role.get_emoji_roles(guild):
+                if reaction.emoji.name == role.get('emoji'):
                     # Removes role to corresponding reacted emoji
 
                     roles = discord.utils.get(guild.roles, name=role.get('name'))
                     await user.remove_roles(roles, reason="Unreacted from react message")
-                    print(f"Removed {role.get('name')} from {user_id}.")
+                    print(f"Removed {role.get('name')} from {reaction.user_id}.")
 
     @commands.Cog.listener()
     async def on_guild_role_create(self, role):
         data = [Roles(id=role.id,
                       guild_id=role.guild.id,
                       name=role.name,
-                      emoji=None,
-                      reaction_message_id=None,
-                      reaction_channel_id=None)]
+                      emoji=None)]
 
         await db.db_handler.insert(data)
-        # current_config = Config(role.guild)
-        # current_config.roles[role.id] = {'name': role.name, 'emoji': None}
-        # current_config.update_config()
 
     @commands.Cog.listener()
     async def on_guild_role_delete(self, role):
-        current_config = Config(role.guild)
-
-        await self.role_embed_remove(current_config, role)
-        current_config.roles.pop(f"{role.id}")
-        current_config.update_config()
+        await db.db_handler_role.delete_role(role)
 
     @commands.Cog.listener()
     async def on_guild_role_update(self, before, after):
-        current_config = Config(before.guild)
-
-        config_role = current_config.roles.get(before.id)
-        config_role['name'] = after.name
-
-        current_config.update_config()
+        await db.db_handler_role.update_role_name(before, after.name)
 
     @commands.check(checks.is_bot_enabled)
     @commands.command(name="setchannel", description="Sets a channel for the role react message")
     async def setchannel(self, ctx):
-        current_config = Config(ctx.guild)
-
-        current_config.role_message_channel_id = int(ctx.channel.id)
-
-        current_config.update_config()
-
-        linked_roles = {}
-
-        for role in current_config.roles.values():
-            if role.get('emoji'):
-                linked_roles['name'] = role.get('name')
-                linked_roles['emoji'] = role.get('emoji')
-
         role_embed = discord.Embed(title="Reaction Roles")
 
-        for role in linked_roles.items():
+        for role in await db.db_handler_role.get_emoji_roles(ctx.guild):
             role_embed.add_field(name=f"{role.get('name')}", value=f"{role.get('emoji')}")
 
         await ctx.send("React with the following emojis for the corresponding role:")
         message = await ctx.send(embed=role_embed)
+        for role in await db.db_handler_role.get_emoji_roles(ctx.guild):
+            await message.add_reaction(role.get('emoji'))
+        await db.db_handler_role.set_channel(ctx.guild, ctx.channel, message)
         await ctx.message.delete()
 
-        current_config.role_message_id = int(message.id)
-
-        current_config.update_config()
-
-    # TODO : After role creation, add to config file
     @commands.check(checks.is_bot_enabled)
     @commands.command(name="createrole", description="Creates a role")
     async def createrole(self, ctx, role_name):
@@ -165,6 +121,7 @@ class ReactionRole(commands.Cog):
 
         mentioned_roles = ctx.message.role_mentions
 
+        # Making sure there is a role linked to the command message
         if len(mentioned_roles) > 1:
             await ctx.send("Please only mention one role")
             return
@@ -173,59 +130,60 @@ class ReactionRole(commands.Cog):
 
         roles = await db.db_handler_role.get_emoji_roles(ctx.guild)
 
+        # Verifying the emoji isn't already in use
         if any(emoji == role.get('emoji') for role in roles):
             await ctx.send("That emoji is already in use.")
             return
 
+        # If emoji isn't in use, link it
         else:
+            # Linking emoji in db
             await db.db_handler_role.link_emoji(ctx.guild, mentioned_role, emoji)
-            await ctx.send(f'Successfully linked {mentioned_role.name} to {emoji}.')
-            for role in roles:
-                if role.get('name') == mentioned_role.name and emoji:
-                    role['emoji'] = emoji
-                    await ctx.message.delete()
-                    response = await ctx.send(f"Linked {role.get('name')} with {emoji}")
-                    await response.delete(delay=10)
 
-                    message_id, channel_id = await db.db_handler_role.get_role_message_info(ctx.guild)
+            channel_id, message_id = await db.db_handler_role.get_channel(ctx.guild)
 
-                    if message_id and channel_id:
-                        channel = await self.bot.fetch_channel(channel_id)
-                        message = await channel.fetch_message(message_id)
+            if message_id and channel_id:
+                print('found message_id and channel_id')
+                channel = await self.bot.fetch_channel(channel_id)
+                message = await channel.fetch_message(message_id)
 
-                        role_embed = message.embeds[0]
-                        role_embed.add_field(name=f"{role.get('name')}", value=f"{role.get('emoji')}")
-                        await message.edit(embed=role_embed)
+                role = await db.db_handler_role.get_role(mentioned_role)
 
-                        await message.add_reaction(emoji)
+                role_embed = message.embeds[0]
+                role_embed.add_field(name=f"{role.name}", value=f"{role.emoji}")
+                await message.edit(embed=role_embed)
 
-                        break
+                await message.add_reaction(emoji)
+                await ctx.message.delete()
 
-                    else:
-                        return
-
+    # TODO: Some of this seems redundant with embed remove, look into that
     @commands.check(checks.is_bot_enabled)
     @commands.command(name="unlinkemoji", description="Unlinks an emoji from a role for the role reaction message")
     async def unlinkemoji(self, ctx, emoji, role=None):
-        current_config = Config(ctx.guild)
 
-        unlinked_role = await self.role_embed_remove(current_config, emoji=emoji)
-        channel = await self.bot.fetch_channel(current_config.role_message_channel_id)
-        message = await channel.fetch_message(current_config.role_message_id)
+        unlinked_role = await self.role_embed_remove(ctx.guild, emoji=emoji)
 
-        await message.clear_reaction(emoji)
-        for role in current_config.roles.values():
-            if emoji == role.get('emoji'):
-                role['emoji'] = None
+        channel_id, message_id = await db.db_handler_role.get_channel(ctx.guild)
 
-        await ctx.message.delete()
-        response = await ctx.send(f"Unlinked {unlinked_role.get('emoji')} from {unlinked_role.get('name')}.")
-        await response.delete(delay=10)
+        if channel_id and message_id:
+            channel = await self.bot.fetch_channel(channel_id)
+            message = await channel.fetch_message(message_id)
+
+            await message.clear_reaction(emoji)
+            await ctx.message.delete()
+
+            unlinked_role = None
+            for role in await db.db_handler_role.get_emoji_roles(ctx.guild):
+                if role.get('emoji') == emoji:
+                    unlinked_role = role
+
+            response = await ctx.send(f"Unlinked {unlinked_role.get('emoji')} from {unlinked_role.get('name')}.")
+            await response.delete(delay=10)
 
         if not unlinked_role:
             await ctx.send(f"Couldn't find a role linked with {emoji}.")
 
-        current_config.update_config()
+        await db.db_handler_role.unlink_emoji(ctx.guild, emoji)
 
 
 def setup(bot):
--- db/db_handler_role.py
+++ db/db_handler_role.py
@@ -7,12 +7,22 @@ async def link_emoji(guild, role, emoji):
         await c.run_sync(local_link_emoji, guild.id, role.id, emoji)
 
 
-# TODO: Add check for single emoji use per role (Add emoji_exists function?)
 def local_link_emoji(session, guild_id, role_id, emoji):
     row = session.query(Roles).filter(Roles.guild_id == guild_id, Roles.id == role_id).one_or_none()
     if row:
         row.emoji = emoji
         session.commit()
+
+
+async def unlink_emoji(guild, emoji):
+    async with await db_handler.connection() as c:
+        await c.run_sync(local_unlink_emoji, guild.id, emoji)
+
+
+def local_unlink_emoji(session, guild_id, emoji):
+    row = session.query(Roles).filter(Roles.guild_id == guild_id, Roles.emoji == emoji).one_or_none()
+    row.emoji = None
+    session.commit()
 
 
 async def get_emoji_roles(guild):
@@ -44,3 +54,56 @@ async def get_role_message_info(guild):
 def local_get_role_message_info(session, guild_id):
     row = session.query(Configs).filter(Configs.id == guild_id).one_or_none()
     return row.reaction_message_id, row.reaction_channel_id
+
+
+async def get_role(role):
+    async with await db_handler.connection() as c:
+        return await c.run_sync(local_get_role, role.id)
+
+
+def local_get_role(session, role_id):
+    row = session.query(Roles).filter(Roles.id == role_id).one_or_none()
+    return row
+
+
+async def update_role_name(role, new_name):
+    async with await db_handler.connection() as c:
+        return await c.run_sync(local_update_role_name, role.id, new_name)
+
+
+def local_update_role_name(session, role_id, new_name):
+    row = local_get_role(session, role_id)
+    row.name = new_name
+    row.commit()
+
+
+async def delete_role(role):
+    async with await db_handler.connection() as c:
+        return await c.run_sync(local_delete_role, role.id)
+
+
+def local_delete_role(session, role_id):
+    session.query(Roles).filter(Roles.id == role_id).one_or_none().delete()
+    session.commit()
+
+
+async def set_channel(guild, channel, message):
+    async with await db_handler.connection() as c:
+        return await c.run_sync(local_set_channel, guild.id, channel.id, message.id)
+
+
+def local_set_channel(session, guild_id, channel_id, message_id):
+    row = session.query(Configs).filter(Configs.id == guild_id).one_or_none()
+    row.reaction_channel_id = channel_id
+    row.reaction_message_id = message_id
+    session.commit()
+
+
+async def get_channel(guild):
+    async with await db_handler.connection() as c:
+        return await c.run_sync(local_get_channel, guild.id)
+
+
+def local_get_channel(session, guild_id):
+    row = session.query(Configs).filter(Configs.id == guild_id).one_or_none()
+    return row.reaction_channel_id, row.reaction_message_id

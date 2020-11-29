e03725fdf5811f73f54b7e7001410c1620edddb4
Psuedoo <43244534+Psuedoo@users.noreply.github.com>
2020-11-29 01:03:10 -0500

    refactored sound.py

--- cogs/sound.py
+++ cogs/sound.py
@@ -14,66 +14,6 @@ from db.models import Sounds
 from db.models import SoundsAssociation
 
 
-def instantiate_configs(guilds):
-    return [Config(guild) for guild in guilds if guild]
-
-
-def instantiate_dbs(guilds):
-    return [TinyDB(Config(guild).sounds) for guild in guilds if guild]
-
-
-class SoundFile:
-    def __init__(self, guild, command_name=None, url=None, title=None):
-        self.command_name = command_name
-        self.url = url
-        self.guild = guild
-        self.guild_id = guild.id
-        self.config = Config(guild)
-        self.path = Path.cwd() / 'sounds'
-        if not self.path.is_dir():
-            self.path.mkdir()
-
-        if title:
-            self.title = title
-        else:
-            self.title = '%(title)s'
-
-        self.file_path = self.path / self.title
-        self.db_path = self.path / f'sounds_{self.guild_id}.json'
-
-        self.ydl_opts = {'format': 'bestaudio',
-                         'noplaylist': True,
-                         'outtmpl': f'{self.file_path}.%(ext)s',
-                         'postprocessors': [{'key': 'FFmpegExtractAudio'}]}
-
-        # self.db = TinyDB(self.db_path)
-        # self.config.sounds = self.db_path
-        # self.config.update_config()
-
-    def download_sound(self):
-        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
-            ydl.download([self.url])
-
-        for file in self.path.iterdir():
-            if file.stem == self.file_path.stem:
-                self.file_path = file
-
-    def add_command(self):
-        if not self.file_path.is_file():
-            self.download_sound()
-
-        if self.command_name in [sound.get('command_name') for sound in self.db]:
-            return
-        else:
-
-            config = Config(self.guild)
-            command_info = {'file': str(self.file_path),
-                            'command_name': str(self.command_name)}
-
-            self.db.insert(command_info)
-            config.update_config()
-
-
 class Sound(commands.Cog):
     """*Commands for sound functionality*"""
 
@@ -126,7 +66,6 @@ class Sound(commands.Cog):
 
         return db.search(Command.command_name == sound)[0]['file']
 
-    # TODO: Add check to see if url is already in db. If so: just add an association
     def download_sound(self, url, file_path):
         with youtube_dl.YoutubeDL({'format': 'bestaudio',
                                    'noplaylist': True,
@@ -220,6 +159,7 @@ class Sound(commands.Cog):
         else:
             await ctx.send(f'Successfully deleted {command_name}')
 
+    # TODO : Make viewsounds look prettier
     @commands.check(checks.is_bot_enabled)
     @commands.command(name="viewsounds", description="Shows the sounds in the sound library")
     async def viewsounds(self, ctx):

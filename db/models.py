eb4ee9f1969df5a93352f21d0302f8e22de2816f
Psuedoo <43244534+Psuedoo@users.noreply.github.com>
2020-12-14 23:58:27 -0500

    updated models

--- db/models.py
+++ db/models.py
@@ -11,7 +11,6 @@ class Guilds(Base):
     id = Column(BigInteger, primary_key=True)
     name = Column(String(20))
     owner_id = Column(BigInteger)
-    twitch_channel = Column(String)
     config = relationship('Configs', backref='guilds', cascade='all, delete')
     twitch = relationship('Twitch', backref='twitch', cascade='all, delete')
     roles = relationship('Roles', backref='guilds', cascade='all, delete')
@@ -93,6 +92,7 @@ class Twitch(Base):
     name = Column(String, primary_key=True)
     shoutout_message = Column(String)
     guild_invite_link = Column(String)
+    guild_invite_message = Column(String)
     guild_id = Column(BigInteger, ForeignKey('guilds.id'))
     sounds = relationship('SoundsAssociation', backref='twitch', cascade='all, delete')
     quotes = relationship('Quotes', backref='twitch', cascade='all, delete')
@@ -122,7 +122,7 @@ if __name__ == '__main__':
                 tbl.drop(engine)
 
 
-    engine = create_engine('postgresql://psuedo@192.168.0.179/discordbot_dev', echo=True)
+    engine = create_engine('postgresql://psuedo@192.168.0.180/discordbot_dev', echo=True)
     user_input = input("1. Create Tables\n"
                        "2. Delete Tabels\n"
                        "3. Renew Tables\n> ")
